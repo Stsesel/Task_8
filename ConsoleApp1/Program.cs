aec72297d294fd4d36a92239600b9e89704e6a38
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2021-12-31 14:30:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\sergkwork\OneDrive\Autodesk_ITMO_2021";
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+            }
+
+            string[] files = Directory.GetFiles(path);
+
+            Console.WriteLine();
+
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+
+
+}
---
+++ Task 8_2a/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8_2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+
+
+            string path2 = "Logs/Number.txt";
+            if (!File.Exists(path2))
+            {
+                File.Create(path2);
+            }
+
+
+            int sum = 0;
+            int num = 0;
+
+            string path3 = "Logs/Number.txt";
+            using (StreamWriter sw = new StreamWriter(path3))
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    num = rnd.Next(-100, 101);
+                    Console.WriteLine("{0,4}", num);
+                    sw.WriteLine("{0,4}", num);
+                    sum = sum + num;
+                }
+
+            }
+            Console.WriteLine("Cумма десяти случайных чисел равна = {0}", sum);
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+}
